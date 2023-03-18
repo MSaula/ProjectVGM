9c541bae9eda65b7b8c866870a9b7f01c2cd3adc
Miquel Saula <miquelsaula@gmail.com>
2023-03-19 00:47:24 +0100

    Mejoras del codigo para hacer scraping de la data de Reddit #9

--- Reddit/RedditDownload.py
+++ Reddit/RedditDownload.py
@@ -8,38 +8,44 @@ import requests
 def fetch_comments(base_url, stock_aliases, start_date, end_date, batch_size, delta):
     all_comments = []
 
-    search_query = '|'.join(stock_aliases)  # Create a search query with stock aliases
-
     current_start_date = start_date
     current_end_date = current_start_date - delta
 
     while current_end_date >= end_date:
-        ref = time.time()
+        results_buffer = {}
+        for word in stock_aliases:
+            ref = time.time()
 
-        print(" -> New batch. From previous %d to %d previous days (n: %d)" %
-              (current_start_date, current_end_date, len(all_comments)))
+            print(" -> New batch. From previous %d to %d previous days. Looking for word %s (n: %d / %d)" %
+                  (current_start_date, current_end_date, word, len(all_comments), len(results_buffer)))
 
-        params = {
-            'q': search_query,
-            'after': str(current_start_date) + 'd',
-            'before': str(current_end_date) + 'd',
-            'size': batch_size,
-            'sort_type': 'score',
-            'sort': 'score'
-        }
-        response = requests.get(base_url, params=params)
+            params = {
+                'q': word,
+                'after': str(current_start_date) + 'd',
+                'before': str(current_end_date) + 'd',
+                'size': batch_size,
+                'sort_type': 'score',
+                'sort': 'score'
+            }
+            response = requests.get(base_url, params=params)
 
-        if response.status_code == 200:
-            data = response.json()
-            comments = data['data']
-            all_comments.extend(comments)
-        else:
-            print(f"Error {response.status_code} occurred while fetching comments")
+            if response.status_code == 200:
+                data = response.json()
+
+                # Se van recogiendo los valores en un diccionario para evitar duplicados
+                for value in data['data']:
+                    results_buffer[value['id']] = value
+            else:
+                print(f"Error {response.status_code} occurred while fetching comments.")
+
+            time.sleep(max(.0, 1.0 - (time.time() - ref)))
+
+        results = list(results_buffer.values())
+        results.sort(key=lambda x: -x['score'])
+        all_comments += results[:batch_size]
 
         current_start_date -= delta
         current_end_date -= delta
-
-        time.sleep(max(.0, 1.0 - (time.time() - ref)))
 
     # Sort the results in ascending order by the 'created_utc' field
     all_comments.sort(key=lambda x: x['created_utc'])
@@ -95,16 +101,16 @@ def write_posts_to_csv(comments, filename):
 
 def main():
     stocks = {
-        'AAPL': ['AAPL', 'Apple', 'iPhone', 'Tim Cook'],
+        'AAPL': ['AAPL', 'Tim Cook'],
         'MSFT': ['MSFT', 'Microsoft', 'Satya Nadella'],
         'AMZN': ['AMZN', 'Amazon', 'Jeff Bezos'],
         'SPY': ['SPY', 'S&P 500'],
         'GOOG': ['GOOG', 'Google', 'Alphabet inc.', 'Sundar Pichai'],
         'TSLA': ['TSLA', 'Tesla', 'Elon Musk'],
         'BBRK': ['BBRK', 'Berkshire Hathaway'],
-        'JPM': ['JPM', 'JPMorgan Chase', 'JPMorgan', 'Jamie Dimon'],
-        'T': ['AT&T', 'John T. Stankey', 'John Stankey'],
+        'JPM': ['JPM', 'JPMorgan', 'Jamie Dimon'],
         'CVX': ['CVX', 'Chevron', 'Mike Wirth'],
+        'T': ['AT&T', 'John T. Stankey', 'John Stankey'],
     }
 
     params = {
@@ -117,20 +123,22 @@ def main():
     for stock, aliases in stocks.items():
         print("#" * 75)
 
-        print("Processing %s comments" % stock)
+        print("Fetching %s comments" % stock)
         comments = fetch_comments(
             base_url="https://api.pushshift.io/reddit/search/comment/",
             stock_aliases=aliases,
             **params
         )
+        print("Writing %d %s comments" % (len(comments), stock))
         write_comments_to_csv(comments, f"{stock}_comments.csv")
 
-        print("Processing %s submissions" % stock)
+        print("Fetching %s submissions" % stock)
         submissions = fetch_comments(
             base_url="https://api.pushshift.io/reddit/search/submission/",
             stock_aliases=aliases,
             **params
         )
+        print("Writing %d %s comments" % (len(submissions), stock))
         write_posts_to_csv(submissions, f"{stock}_submissions.csv")
 
 
